4b7dfedec1b5c334b7bfd531c131adeff3b0c116
Mark Hakansson <mark.hakansson@live.se>
2021-01-26 08:06:42 +0100

    Update blinky with rtt

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,22 @@
 #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
- 
+
 use panic_halt as _;
-use rtt_target::{rprintln, rtt_init_print};
-use stm32f4xx_hal::{
-    prelude::*,
-    gpio::*,
-};
 use rtic::cyccnt::{Instant, U32Ext};
+use rtt_target::{rprintln, rtt_init_print};
+use stm32f4xx_hal::{gpio::*, prelude::*};
+use cortex_m::asm;
 
 const PERIOD: u32 = 8_000_000;
 
 #[rtic::app(device = stm32f4xx_hal::stm32, monotonic = rtic::cyccnt::CYCCNT, peripherals = true)]
 const APP: () = {
     struct Resources {
-        led: gpioa::PA5<Output<PushPull>>
+        led: gpioa::PA5<Output<PushPull>>,
     }
-
+    
+    #[inline(never)]
     #[init(schedule = [led_on])]
     fn init(mut cx: init::Context) -> init::LateResources {
         rtt_init_print!();
@@ -31,15 +30,14 @@ const APP: () = {
         // Initialize cyccnt
         cx.core.DCB.enable_trace();
         cx.core.DWT.enable_cycle_counter();
-        
+
         // Schedule led to turn on
         cx.schedule.led_on(cx.start + PERIOD.cycles()).unwrap();
 
-        init::LateResources{
-            led
-        }
+        init::LateResources { led }
     }
-    
+
+    #[inline(never)]
     #[task(schedule = [led_off], resources = [led])]
     fn led_on(cx: led_on::Context) {
         rprintln!("led_on");
@@ -47,11 +45,19 @@ const APP: () = {
         cx.resources.led.set_high().unwrap();
     }
 
+    #[inline(never)]
     #[task(schedule = [led_on], resources = [led])]
     fn led_off(cx: led_off::Context) {
         rprintln!("led_off");
         cx.schedule.led_on(cx.scheduled + PERIOD.cycles()).unwrap();
         cx.resources.led.set_low().unwrap();
+    }
+
+    #[idle]
+    fn idle(_cx: idle::Context) -> ! {
+        loop {
+            asm::nop();
+        }
     }
 
     extern "C" {
